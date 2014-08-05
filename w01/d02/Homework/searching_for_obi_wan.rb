e80bfc0acf12ec06115ddf0ed89b61f56c2ec9d6
mconiaris <mconiaris@gmail.com>
2014-08-05 19:52:22 -0400

    Attempted to fix errors in user entries. I used the wrong commands to downcase the text.

--- w01/d02/Homework/searching_for_obi_wan.rb
+++ w01/d02/Homework/searching_for_obi_wan.rb
@@ -3,12 +3,12 @@ puts "What is your name?"
 name = gets.chomp
 puts "It is a pleasure to meet you #{name}. Have you ever met a protocol droid before?"
 user_answer = gets.chomp
-puts "#{user_answer}.upcase? How interesting, for someone from around these parts."
+puts "#{user_answer.upcase}? How interesting, for someone from around these parts."
 puts "I'm terribly sorry for prying, but you don't by any chance go by the alias of Obi-Wan Kenobi, do you? (Answer \"I do\" or \"I don't\")"
 kenobi_check = gets.chomp
-if kenobi_check.underscore == "i do"
+if kenobi_check.downcase == "i do"
   puts "Oh, marvelous! Simply marvelous! Say hello to RD-D2; he's been looking all over for you."
-elsif kenobi_check.underscore == "i don't"
+elsif kenobi_check.downcase == "i don't"
   puts "I've really enjoyed speaking with you, #{name}, but if you'll please excuse me, I have to help my friend find someone named Obi-Wan Kenobi."
 else
   puts "I'm sorry, I do not understand your answer. I think that I will leave you alone now."
