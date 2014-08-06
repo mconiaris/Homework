8b0971c1aba088deeedc5372b2bfbb673ad7201e
mconiaris <mconiaris@gmail.com>
2014-08-05 23:01:50 -0400

    Removed redundant puts statement. Put statements included in case statements. (Not the best design, but I will handle that later.) Fixed syntax so that things like a.to_i and b.to_i is amended.

--- w01/d02/Homework/calculator.rb
+++ w01/d02/Homework/calculator.rb
@@ -1,22 +1,23 @@
-result = nil
+c = 0
 print "Please select a number: "
 a = gets.chomp
-print "\nPlease select + (to add), - (to subtract), * to multiply or / (to divide)"
-operaton = gets.chomp
-case operation
+print "Please select a second number: "
+b = gets.chomp
+print "Please select + (to add), - (to subtract), * to multiply or / (to divide)"
+op = gets.chomp
+case op
   when "+"
-    result = a + b
+    c = a.to_i + b.to_i
+    puts "#{a} + #{b} = #{c}"
   when "-"
-    result = a - b
+    c = a.to_i - b.to_i
+    puts "#{a} + #{b} = #{c}"
   when "*"
-    result = a * b
+    c = a.to_i * b.to_i
+    puts "#{a} + #{b} = #{c}"
   when "/"
-    result = a / b
+    c = a.to_i / b.to_i
+    puts "#{a} + #{b} = #{c}"
   else
     puts "I did not understand which operation you would like to execute."
 end
-print "\nPlease select a second number: "
-b = gets.chomp
-
-
-puts "#{a} #{operaton} #{b} = result"
