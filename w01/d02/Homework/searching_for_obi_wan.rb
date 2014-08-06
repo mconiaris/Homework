7a71babdce46cb6278971a9a313098f561155fde
mconiaris <mconiaris@gmail.com>
2014-08-05 22:17:24 -0400

    Fixed a typo in the capitalize method.

--- w01/d02/Homework/searching_for_obi_wan.rb
+++ w01/d02/Homework/searching_for_obi_wan.rb
@@ -3,7 +3,7 @@ puts "What is your name?"
 name = gets.chomp
 puts "It is a pleasure to meet you #{name}. Have you ever met a protocol droid before?"
 user_answer = gets.chomp
-puts "#{user_answer.capitali}? How interesting, for someone from around these parts."
+puts "#{user_answer.capitalize}? How interesting, for someone from around these parts."
 puts "I'm terribly sorry for prying, but you don't by any chance go by the alias of Obi-Wan Kenobi, do you? (Answer \"I do\" or \"I don't\")"
 kenobi_check = gets.chomp
 if kenobi_check.downcase == "i do"
